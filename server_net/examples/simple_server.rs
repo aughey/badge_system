be499da5f60f104369900e5a8ea27b7d1bd11ab3
John Aughey <jha@aughey.com>
2024-04-11 23:27:47 +0000

    asdf

--- server_net/examples/simple_server.rs
+++ server_net/examples/simple_server.rs
@@ -7,14 +7,17 @@
 //! Note that `unwrap()` is used to deal with networking errors; this is not something
 //! that is sensible outside of example code.
 
+use rustls::crypto::{aws_lc_rs as provider, CryptoProvider};
 use std::env;
 use std::error::Error as StdError;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
-use std::net::TcpListener;
 use std::sync::Arc;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpListener;
 
-fn main() -> Result<(), Box<dyn StdError>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn StdError>> {
     let mut args = env::args();
     args.next();
     let cert_file = args.next().expect("missing certificate file argument");
@@ -25,33 +28,55 @@ fn main() -> Result<(), Box<dyn StdError>> {
     let private_key =
         rustls_pemfile::private_key(&mut BufReader::new(&mut File::open(private_key_file)?))?
             .unwrap();
-    let config = rustls::ServerConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(certs, private_key)?;
+    let config = rustls::ServerConfig::builder_with_provider(
+        CryptoProvider {
+            cipher_suites: [
+                provider::cipher_suite::TLS13_AES_256_GCM_SHA384,
+                provider::cipher_suite::TLS13_AES_128_GCM_SHA256,
+                provider::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+            ]
+            .to_vec(),
+            ..provider::default_provider()
+        }
+        .into(),
+    )
+    .with_protocol_versions(&[&rustls::version::TLS13])?
+    .with_no_client_auth()
+    .with_single_cert(certs, private_key)?;
 
-    let listener = TcpListener::bind(format!("[::]:{}", 4443)).unwrap();
-    let (mut stream, _) = listener.accept()?;
+    let listener = TcpListener::bind(format!("[::]:{}", 4443)).await?;
+    let acceptor = tokio_rustls::TlsAcceptor::from(Arc::new(config));
+    let (stream, _) = listener.accept().await?;
+    // turn stream into an async stream
+    let mut stream = acceptor.accept(stream).await?;
 
-    let mut conn = rustls::ServerConnection::new(Arc::new(config))?;
-    println!("Starting handshake");
-    conn.complete_io(&mut stream)?;
-    println!("Handshake complete");
+    // // Get past the handshake
+    // while conn.is_handshaking() {
+    //     conn.complete_io(&mut stream)?;
+    // }
+    // println!("alpn protocol: {:?}", conn.alpn_protocol());
+    // println!("cipher suite: {:?}", conn.negotiated_cipher_suite());
+    // println!("protocol version: {:?}", conn.protocol_version());
 
-    conn.writer().write_all(b"Hello from the server")?;
-    println!("Sent message to client");
-    conn.complete_io(&mut stream)?;
-    println!("Completed IO");
     loop {
         let mut buf = [0u8; 1024];
-        match conn.reader().read(&mut buf) {
+
+        match stream.read(&mut buf).await {
             Ok(0) => break,
-            Ok(len) => println!("read {} bytes", len),
+            Ok(len) => {
+                let buf = std::str::from_utf8(&buf[..len]).unwrap();
+                println!("read {} bytes: {}", len, buf);
+                break;
+            }
             Err(e) => {
                 eprintln!("read error: {:?}", e);
-                //break;
+                break;
             }
         }
     }
+    // println!("alpn protocol: {:?}", stream.conn.alpn_protocol());
+    // println!("cipher suite: {:?}", stream.conn.negotiated_cipher_suite());
+    // println!("protocol version: {:?}", stream.conn.protocol_version());
 
     Ok(())
 }
