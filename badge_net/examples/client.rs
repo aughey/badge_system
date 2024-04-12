f556d630e180d3102610a70895707c41d20aa3f8
John Aughey <jha@aughey.com>
2024-04-12 12:32:55 +0000

    async all the way

--- badge_net/examples/client.rs
+++ badge_net/examples/client.rs
@@ -1,18 +1,20 @@
 use anyhow::Result;
-use embedded_tls::{blocking::TlsConnection, TlsConfig, TlsContext};
-use rand::{rngs::OsRng, RngCore};
-use tokio::signal;
+use embedded_tls::{TlsConfig, TlsConnection, TlsContext};
+use rand::rngs::OsRng;
+mod from_tokio;
 
-use std::net::TcpStream;
+use tokio::net::TcpStream;
 
-fn main() -> Result<()> {
-    let client = TcpStream::connect("127.0.0.1:4443")?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = TcpStream::connect("127.0.0.1:4443").await?;
 
     let config = TlsConfig::new().enable_rsa_signatures();
     let mut read_record_buffer = [0u8; 16384];
     let mut write_record_buffer = [0u8; 16384];
     let mut tls = TlsConnection::new(
-        embedded_io_adapters::std::FromStd::new(client),
+        from_tokio::FromTokio::new(client),
+        //embedded_io_adapters::std::FromStd::new(client),
         &mut read_record_buffer,
         &mut write_record_buffer,
     );
@@ -21,6 +23,7 @@ fn main() -> Result<()> {
         &config,
         embedded_tls::UnsecureProvider::new::<embedded_tls::Aes128GcmSha256>(OsRng),
     ))
+    .await
     .map_err(|e| anyhow::anyhow!("Failed to open connection: {:?}", e))?;
 
     println!("TLS connection established!");
@@ -28,14 +31,17 @@ fn main() -> Result<()> {
     let mut buf = [0u8; 1024];
     let size = tls
         .read(&mut buf)
+        .await
         .map_err(|e| anyhow::anyhow!("Failed to read data: {:?}", e))?;
     let buf = std::str::from_utf8(&buf[..size]).unwrap();
     println!("read {} bytes: {}", size, buf);
 
     tls.write("Hello World from client".as_bytes())
+        .await
         .map_err(|e| anyhow::anyhow!("Failed to write data: {:?}", e))?;
 
     tls.close()
+        .await
         .map_err(|(_, e)| anyhow::anyhow!("Failed to close connection: {:?}", e))?;
 
     Ok(())
