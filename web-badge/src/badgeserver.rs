f1bb855a536fc15185ad57c8d1483ea9cf3c04a9
John Aughey <jha@aughey.com>
2024-04-24 13:18:12 +0000

    disabling tls, this will be handled by the proxy

--- web-badge/src/badgeserver.rs
+++ web-badge/src/badgeserver.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
-use rustls::crypto::{aws_lc_rs as provider, CryptoProvider};
-use rustls::server::WebPkiClientVerifier;
-use rustls::RootCertStore;
+// use rustls::crypto::{aws_lc_rs as provider, CryptoProvider};
+// use rustls::server::WebPkiClientVerifier;
+// use rustls::RootCertStore;
 use std::fs::File;
 use std::io::BufReader;
 use std::sync::Arc;
@@ -14,70 +14,70 @@ pub async fn server(
     get_frequency: impl Fn() -> Option<u64> + Send + 'static + Clone,
     get_text: impl Fn() -> Option<String> + Send + 'static + Clone,
 ) -> Result<()> {
-    let mut args = args.into_iter();
-    args.next();
-    let ca_file = args
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("missing ca file argument"))?;
-    let cert_file = args
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("missing certificate file argument"))?;
-    let private_key_file = args
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("missing private key file argument"))?;
+    // let mut args = args.into_iter();
+    // args.next();
+    // let ca_file = args
+    //     .next()
+    //     .ok_or_else(|| anyhow::anyhow!("missing ca file argument"))?;
+    // let cert_file = args
+    //     .next()
+    //     .ok_or_else(|| anyhow::anyhow!("missing certificate file argument"))?;
+    // let private_key_file = args
+    //     .next()
+    //     .ok_or_else(|| anyhow::anyhow!("missing private key file argument"))?;
 
-    info!("Loading CA file: {:?}", ca_file);
-    let roots = {
-        let mut filebuf = BufReader::new(
-            File::open(&ca_file).map_err(|e| anyhow::anyhow!("Could not open {ca_file}: {e:?}"))?,
-        );
-        let root_ca = rustls_pemfile::certs(&mut filebuf);
-        let mut roots = RootCertStore::empty();
-        for cert in root_ca {
-            roots
-                .add(cert?)
-                .map_err(|_| anyhow::anyhow!("failed to add cert to root store"))?;
-        }
-        roots
-    };
+    // info!("Loading CA file: {:?}", ca_file);
+    // let roots = {
+    //     let mut filebuf = BufReader::new(
+    //         File::open(&ca_file).map_err(|e| anyhow::anyhow!("Could not open {ca_file}: {e:?}"))?,
+    //     );
+    //     let root_ca = rustls_pemfile::certs(&mut filebuf);
+    //     let mut roots = RootCertStore::empty();
+    //     for cert in root_ca {
+    //         roots
+    //             .add(cert?)
+    //             .map_err(|_| anyhow::anyhow!("failed to add cert to root store"))?;
+    //     }
+    //     roots
+    // };
 
-    info!("Loading cert file: {:?}", cert_file);
-    let certs = rustls_pemfile::certs(&mut BufReader::new(
-        &mut File::open(&cert_file)
-            .map_err(|e| anyhow::anyhow!("Could not open {cert_file}: {e:?}"))?,
-    ))
-    .collect::<Result<Vec<_>, _>>()?;
-    info!("Loading private key file: {:?}", private_key_file);
-    let private_key =
-        rustls_pemfile::private_key(&mut BufReader::new(&mut File::open(&private_key_file)?))?
-            .ok_or_else(|| {
-                anyhow::anyhow!(
-                    "no private key found in {:?} (encrypted keys not supported)",
-                    private_key_file
-                )
-            })?;
+    // info!("Loading cert file: {:?}", cert_file);
+    // let certs = rustls_pemfile::certs(&mut BufReader::new(
+    //     &mut File::open(&cert_file)
+    //         .map_err(|e| anyhow::anyhow!("Could not open {cert_file}: {e:?}"))?,
+    // ))
+    // .collect::<Result<Vec<_>, _>>()?;
+    // info!("Loading private key file: {:?}", private_key_file);
+    // let private_key =
+    //     rustls_pemfile::private_key(&mut BufReader::new(&mut File::open(&private_key_file)?))?
+    //         .ok_or_else(|| {
+    //             anyhow::anyhow!(
+    //                 "no private key found in {:?} (encrypted keys not supported)",
+    //                 private_key_file
+    //             )
+    //         })?;
 
-    info!("Building client verifier");
-    let client_verifier = WebPkiClientVerifier::builder(roots.into()).build()?;
+    // info!("Building client verifier");
+    // let client_verifier = WebPkiClientVerifier::builder(roots.into()).build()?;
 
-    let config = rustls::ServerConfig::builder_with_provider(
-        CryptoProvider {
-            cipher_suites: [
-                provider::cipher_suite::TLS13_AES_256_GCM_SHA384,
-                provider::cipher_suite::TLS13_AES_128_GCM_SHA256,
-                provider::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
-            ]
-            .to_vec(),
-            ..provider::default_provider()
-        }
-        .into(),
-    )
-    .with_protocol_versions(&[&rustls::version::TLS13])?
-    .with_client_cert_verifier(client_verifier)
-    .with_single_cert(certs, private_key)?;
+    // let config = rustls::ServerConfig::builder_with_provider(
+    //     CryptoProvider {
+    //         cipher_suites: [
+    //             provider::cipher_suite::TLS13_AES_256_GCM_SHA384,
+    //             provider::cipher_suite::TLS13_AES_128_GCM_SHA256,
+    //             provider::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+    //         ]
+    //         .to_vec(),
+    //         ..provider::default_provider()
+    //     }
+    //     .into(),
+    // )
+    // .with_protocol_versions(&[&rustls::version::TLS13])?
+    // .with_client_cert_verifier(client_verifier)
+    // .with_single_cert(certs, private_key)?;
+    //let acceptor = tokio_rustls::TlsAcceptor::from(Arc::new(config));
 
     let listener = TcpListener::bind(format!("[::]:{}", 4443)).await?;
-    let acceptor = tokio_rustls::TlsAcceptor::from(Arc::new(config));
 
     loop {
         info!("Waiting for incoming connection");
@@ -85,13 +85,13 @@ pub async fn server(
         info!("Got incoming connection");
 
         // turn stream into an async stream
-        let mut stream = match acceptor.accept(stream).await {
-            Ok(stream) => stream,
-            Err(e) => {
-                eprintln!("accept error: {:?}", e);
-                continue;
-            }
-        };
+        // let mut stream = match acceptor.accept(stream).await {
+        //     Ok(stream) => stream,
+        //     Err(e) => {
+        //         eprintln!("accept error: {:?}", e);
+        //         continue;
+        //     }
+        // };
 
         let stream = ReadWriteWrapper { inner: stream };
 
