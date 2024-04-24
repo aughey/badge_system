b6e0b4cd63b25297b23509c4dd4a5f333078aa99
John Aughey <jha@aughey.com>
2024-04-24 15:24:46 +0000

    asdf

--- unsafetest/src/main.rs
+++ unsafetest/src/main.rs
@@ -1,26 +1,34 @@
 use std::mem::MaybeUninit;
 
-struct MyMemoryBuffer {
+pub struct MyMemoryBuffer {
     data: MaybeUninit<[i32; 64]>,
     len: usize,
 }
 impl MyMemoryBuffer {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             data: MaybeUninit::uninit(),
             len: 0,
         }
     }
-    fn push(&mut self, value: i32) {
-        unsafe {
-            self.data.as_mut_ptr().add(self.len).write(value);
+    pub fn push(&mut self, value: i32) -> Option<usize> {
+        // SAFETY: Getting a mutable reference to the array in an unsafe block
+        // The integrity of the initialization and length of the array is maintained within this method.
+        let array = unsafe { &mut *self.data.as_mut_ptr() };
+        if self.len >= array.len() {
+            return None;
+        } else {
+            array[self.len] = value;
             self.len += 1;
         }
+        Some(self.len)
     }
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.len
     }
-    fn as_slice(&self) -> &[i32] {
+    pub fn as_slice(&self) -> &[i32] {
+        // SAFETY: Get a reference to the array in an unsafe block.
+        // len was maintained in push correctly, so the slice is valid.
         let array = unsafe { &*self.data.as_ptr() };
         array[..self.len].as_ref()
     }
